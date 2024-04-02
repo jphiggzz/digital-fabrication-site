e4114710de25230c2568e0e63081abdd7bef61aa
eliasxlevy <elias.levy@vanderbilt.edu>
2024-04-02 08:27:24 -0500

    add services to jest mapper

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     moduleNameMapper: {
       '^@/assets/(.*)$': '<rootDir>/assets/$1',
       '^@/pages/(.*)$': '<rootDir>/pages/$1',
+      '^@/services/(.*)$': '<rootDir>/services/$1',
       '^@/components/(.*)$': '<rootDir>/components/$1',
       '^@/firebase/(.*)$': '<rootDir>/firebase/$1',
     },
